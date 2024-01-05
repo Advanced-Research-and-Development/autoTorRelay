52a951c4c0eaff8c99941aecf947c4ce27e9172b
min <maxati9600@gmail.com>
2024-01-05 16:06:22 +0800

    [fix] fix bug for exit node and command in readme

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ this is an auto script to get top relay
 ## usage
 run for bot
 ```
-bash autoTorRelay.sh bot
+sudo bash autoTorRelay.sh bot
 ```
 run for humam
 ```
-bash autoTorRelay.sh
+sudo bash autoTorRelay.sh
 ```
--- autoTorRelay.sh
+++ autoTorRelay.sh
@@ -1,11 +1,12 @@
 if [ "$1" = "bot" ] ; then
 	echo '[bot mode] do it by bot'
-	curl 'https://onionoo.torproject.org/details?type=relay&order=-consensus_weight&limit=250&running=true' -o /tmp/torRelay.tmp
+	curl -s 'https://onionoo.torproject.org/details?type=relay&order=-consensus_weight&limit=250&running=true' -o /tmp/torRelay.tmp
 	EntryN=$(cat /tmp/torRelay.tmp | awk -F'fingerprint\":\"' '{print $2}' |awk -F'\",' '{print $1}' |grep -v '^$'|awk 'NR==1{print $1}')
 	ExitN=$(cat /tmp/torRelay.tmp | awk -F'fingerprint\":\"' '{print $2}' |awk -F'",' '{print $1}' |grep -v '^$'|awk 'NR==2{print $1}')
+	rm /tmp/torRelay.tmp
 	sed -i '/^EntryNode\|^ExitNode/d' /etc/tor/torrc
 	echo "EntryNodes {$EntryN}">>/etc/tor/torrc
-	echo "ExitNodes {$EntryN}">>/etc/tor/torrc
+	echo "ExitNodes {$ExitN}">>/etc/tor/torrc
 else
 	echo '[User mode] do it by humam!!!'
 	sed -i '/^EntryNode\|^ExitNode/d' /etc/tor/torrc
