c3315f8c68ddf740d180797b559dd34791f03cd8
min <maxati9600@gmail.com>
2024-01-05 16:01:36 +0800

    [code] main code to get Relay from online

---
+++ autoTorRelay.sh
@@ -0,0 +1,13 @@
+if [ "$1" = "bot" ] ; then
+	echo '[bot mode] do it by bot'
+	curl 'https://onionoo.torproject.org/details?type=relay&order=-consensus_weight&limit=250&running=true' -o /tmp/torRelay.tmp
+	EntryN=$(cat /tmp/torRelay.tmp | awk -F'fingerprint\":\"' '{print $2}' |awk -F'\",' '{print $1}' |grep -v '^$'|awk 'NR==1{print $1}')
+	ExitN=$(cat /tmp/torRelay.tmp | awk -F'fingerprint\":\"' '{print $2}' |awk -F'",' '{print $1}' |grep -v '^$'|awk 'NR==2{print $1}')
+	sed -i '/^EntryNode\|^ExitNode/d' /etc/tor/torrc
+	echo "EntryNodes {$EntryN}">>/etc/tor/torrc
+	echo "ExitNodes {$EntryN}">>/etc/tor/torrc
+else
+	echo '[User mode] do it by humam!!!'
+	sed -i '/^EntryNode\|^ExitNode/d' /etc/tor/torrc
+fi
+
